dcdac559f61e49f888e383ca2bb7fe16071bdc4a
AbdulazizAlkhars <alkharswork@gmail.com>
2022-08-11 20:26:11 +0300

    Task Done

--- coffeeshops/signals.py
+++ coffeeshops/signals.py
@@ -1,12 +1,10 @@
 #  import send_mail from django.core.mail (we will need this function later).
+from unicodedata import name
 from django.core.mail import send_mail
 from coffeeshops.models import CafeOwner, CoffeeShop, CoffeeShopAddress, Drink
 from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 from utils import create_slug
-"""
-if created is true, Send an email with the subject as New Cafe Owner and the body of the email as A new cafe owner has joined named <FULL_NAME_OF_CAFE_OWNER>. For the from_email you can use "sender@test.com" and recipient_list can be ["receiver@test.com"].
-"""
 
 
 @receiver(post_save, sender=CafeOwner)
@@ -19,9 +17,6 @@ def send_new_owner_email(sender, instance, created, **kwargs):
             ["receiver@test.com"],
             fail_silently=False,
         )
-# Check if instance does not have a slug
-# If our instance does not have a slug then just set instance.slug equal to create_slug(instance) where create_slug is imported from utils (i.e., from utils import create_slug).
-# We want to make sure that an address object always exists for a CoffeeShop even if one was never created or if it is deleted.
 
 
 @receiver(pre_save, sender=CoffeeShop)
@@ -30,9 +25,6 @@ def slugify_coffee_shop(sender, instance, **kwargs):
         instance.slug = create_slug(instance)
 
 
-# Check if the instance has been created, and if instance.location is empty
-# Set instance.location equal to the new CoffeeShopAddress object you've created.
-# Save your instance.
 @receiver(post_save, sender=CoffeeShop)
 def add_default_address(sender, instance, created, **kwargs):
     if created and not instance.location:
@@ -40,3 +32,24 @@ def add_default_address(sender, instance, created, **kwargs):
             coffee_shop=instance)
         instance.location = createdLocation
         instance.save()
+
+
+@receiver(post_delete, sender=CoffeeShopAddress)
+def restore_default_address(sender, instance, **kwargs):
+    # Get the coffee shop from the deleted instance (hint: use the related name coffee_shop) and store it in a variable called coffee_shop.
+    coffee_shop = instance.coffee_shop
+    # Create a new CoffeeShopAddress instance.
+    defualtLocation = CoffeeShopAddress.objects.create()
+    # Set coffee_shop.location equal to the new CoffeeShopAddress object you've created.
+    coffee_shop.location = defualtLocation
+    # Save your coffee_shop instance.
+    coffee_shop.save()
+
+
+@receiver(pre_save, sender=Drink)
+def slugify_coffee_shop(sender, instance, **kwargs):
+    # Set instance.is_out_of_stock to False if instance.stock_count is greater than zero and vice-versa.
+    if instance.stock_count > 0:
+        instance.is_out_of_stock = True
+    else:
+        instance.is_out_of_stock = False
